8e90a43f61ff41ae0263c100d6e25e1b65d2c0d2
AutumnTroyUTSA <99752934+AutumnTroyUTSA@users.noreply.github.com>
2023-11-30 11:54:45 -0600

    FixedItemsandCart

--- routes/dice.js
+++ routes/dice.js
@@ -50,13 +50,6 @@ router.get('/', function (request, response) {
     response.render('dice', { products });
 });
 
-// Sort products by price
-router.get('/sort-by-price', function (request, response) {
-    const sortedProducts = [...products].sort((a, b) => a.price - b.price);
-    response.render('dice', { products: sortedProducts });
-});
-
-
 // Add item to the cart
 router.post('/add-to-cart/:productId', function (request, response) {
     const productId = parseInt(request.params.productId, 10);
@@ -76,6 +69,7 @@ router.post('/add-to-cart/:productId', function (request, response) {
         response.status(404).send('Product not found');
     }
 });
+
 
 module.exports = router;
 
